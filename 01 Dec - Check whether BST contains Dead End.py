63378578cbe8614f7bbe62c3ff36fc2817e055f1
Eashan Srivastava <75667274+Ventmaster@users.noreply.github.com>
2023-12-01 21:58:44 +0530

    Add files via upload

---
+++ 01 Dec - Check whether BST contains Dead End.py
@@ -0,0 +1,16 @@
+# 01 December 2023
+# Check whether BST contains Dead End
+
+class Solution:
+    def isDeadEnd(self, root):
+        # Code here
+        def deadEnd(root, min_val, max_val):
+            if not root:
+                return False
+            
+            if min_val == max_val:
+                return True
+                
+            return deadEnd(root.left, min_val, root.data-1) or deadEnd(root.right, root.data+1, max_val)
+            
+        return deadEnd(root, 1, float('inf'))
