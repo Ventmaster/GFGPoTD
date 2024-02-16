d429b2101c4e188bb5dbb0274b1db0e9e2008532
Eashan Srivastava <75667274+Ventmaster@users.noreply.github.com>
2024-02-16 11:11:32 +0530

    Add files via upload

---
+++ 16 Feb 24 - Flatten BST to sorted list.py
@@ -0,0 +1,32 @@
+# 16 February 2024
+# Flatten BST to sorted list
+
+import sys
+sys.setrecursionlimit(10**5)
+
+class Solution:
+    def flattenBST(self, root):
+        # code here
+        prev, head = None, root
+        
+        def dfs(node):
+            nonlocal prev, head
+            
+            if not node:
+                return 
+            
+            dfs(node.left)
+            
+            if not prev:
+                head = node
+            else:
+                node.left = None
+                prev.right = node
+                
+            prev = node
+            
+            dfs(node.right)
+            
+            return head
+            
+        return dfs(root)
