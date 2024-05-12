c390905083e56e11bb3b9d832dfe3b7b9ca9f9ec
Eashan Srivastava <75667274+Ventmaster@users.noreply.github.com>
2024-05-12 18:38:09 +0530

    Add files via upload

---
+++ 12 May 24 - Minimum steps to destination.py
@@ -0,0 +1,9 @@
+# 12th May 2024
+# Minimum steps to destination
+
+class Solution:
+    def minSteps(self, d):
+        # code here
+        for i in range(1, 2*d):
+            if (((i * (i + 1)) // 2) + d) % 2 == 0 and ((i * (i+1)) // 2) >= d:
+                return i
