2050e4085e8f8b4d17b39ce7799c7b8ef065d402
Eashan Srivastava <75667274+Ventmaster@users.noreply.github.com>
2024-12-05 23:06:53 +0530

    Add files via upload

---
+++ 05 Dec 24 - Sort 0s, 1s and 2s.py
@@ -0,0 +1,9 @@
+# 05th December 2024
+# Sort 0s, 1s and 2s
+
+class Solution:
+    # Function to sort an array of 0s, 1s, and 2s
+    def sort012(self, arr):
+        # code here
+        arr.sort()
+        return arr
